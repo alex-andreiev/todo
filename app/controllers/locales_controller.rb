e30a6c01116fd59c0fedf04236e4cedb9ab12892
Alex Andreev <alexckua@active-bridge.com>
2018-07-31 18:42:43 +0300

    Skip authenticate for locale controller

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,4 +1,6 @@
 class LocalesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def update
     session[:locale] = params[:id]
     redirect_back fallback_location: root_path
